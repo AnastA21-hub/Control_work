51e518a64cada37417ef643dcc78d1705b2a9430
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2024-01-27 21:13:07 +0300

    Заполнение нового массива

--- Program.cs
+++ Program.cs
@@ -13,3 +13,25 @@ Console.WriteLine();
 Console.Write("Введите длину массива от 0 до 3: ");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] arr = new string[size];
+Random random = new Random();
+if (size <= 3)
+{
+	if (size > 0)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			int index = random.Next(array.Length);
+			arr[i] = array[index];
+
+		}
+		PrintArray(arr);
+	}
+	else
+	{
+		Console.WriteLine("[]");
+	}
+}
+else
+{
+	Console.Write("Неверная длина массива");
+}
