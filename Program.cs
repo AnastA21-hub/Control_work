10ead0292f9f9e055b71d85571c3b12b43a45c2d
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2024-01-27 21:12:13 +0300

    Ввод размера нового массива

--- Program.cs
+++ Program.cs
@@ -8,3 +8,8 @@
 
 string[] array = { "Hello", "2", "world", ":-)" };
 PrintArray(array);
+
+Console.WriteLine();
+Console.Write("Введите длину массива от 0 до 3: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arr = new string[size];
